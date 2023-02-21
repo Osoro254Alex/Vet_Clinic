535bab66b87b33866a49fc26193b3362f8d9d47e
Alex Ogeto <alexogeto54@gmail.com>
2023-02-21 18:22:32 +0300

    Modify schema.sql file

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+create database vet_clinic;
+create table animals(
+    id int GENERATED ALWAYS AS IDENTITY,
+    name varchar(40),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal,
+    primary key (id)
 );
+
