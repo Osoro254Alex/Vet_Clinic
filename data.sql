459bc71b4755921263edf24b5b2021808dacd295
Alex Ogeto <alexogeto54@gmail.com>
2023-02-21 18:15:50 +0300

    Modify data.sql file

--- data.sql
+++ data.sql
@@ -1,5 +1,14 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals (
+ name, date_of_birth, escape_attempts, 
+ neutered, weight_kg) values ('Agumon', 'Feb 3, 2020', 0, true, 10.23);
+insert into animals (
+ name, date_of_birth, escape_attempts, 
+ neutered, weight_kg) values ('Gabumon', 'Nov 15, 2018',  2, true, 8);
+insert into animals (
+ name, date_of_birth, escape_attempts, 
+ neutered, weight_kg) values ('Pikachu', 'Jan 7, 2021', 1, false, 15.04);
+insert into animals (
+ name, date_of_birth, escape_attempts, 
+ neutered, weight_kg) values ('Devimon', 'May 12, 2017', 5, true, 11);
